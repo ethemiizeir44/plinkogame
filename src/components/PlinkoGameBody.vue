d770985302f8b07c73392afe796847348fab3d2e
Izeir <ethemiizeir44@gmail.com>
2023-08-10 12:21:19 -0700

    refactored repo

--- src/components/PlinkoGameBody.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-const { count, inc, dec } = useCount();
-</script>
-
-<template>
-    <div id="plinko" />
-</template>
--- src/server/graphql/index.ts
+++
@@ -1,8 +0,0 @@
-import type { NonEmptyArray } from 'type-graphql'
-import hello from './hello/resolver'
-import login from './login/resolver'
-const resolvers: NonEmptyArray<Function> = [
-  hello,
-  login,
-]
-export default resolvers
